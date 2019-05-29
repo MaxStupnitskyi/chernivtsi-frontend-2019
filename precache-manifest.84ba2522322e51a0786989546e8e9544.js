a44ee0889a0427bb7561784a406daa8d162d5ce4
Max Stupnitskyi <maxstupnitskyi@gmail.com>
2019-05-29 13:34:38 +0300

    Updates

---
+++ precache-manifest.84ba2522322e51a0786989546e8e9544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0721f6fb84501fe52fb3b64c33996bd",
+    "url": "/MaxStupnitskyi/react-router.git/index.html"
+  },
+  {
+    "revision": "6d967452b2a0c7a3f2df",
+    "url": "/MaxStupnitskyi/react-router.git/static/css/main.ee569f91.chunk.css"
+  },
+  {
+    "revision": "fd2041e034f334722d67",
+    "url": "/MaxStupnitskyi/react-router.git/static/js/2.287283f5.chunk.js"
+  },
+  {
+    "revision": "6d967452b2a0c7a3f2df",
+    "url": "/MaxStupnitskyi/react-router.git/static/js/main.8c25afd2.chunk.js"
+  },
+  {
+    "revision": "2e48798ffac4ec980d67",
+    "url": "/MaxStupnitskyi/react-router.git/static/js/runtime~main.c891800e.js"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/MaxStupnitskyi/react-router.git/static/media/github-logo.76d344f0.svg"
+  }
+]);
